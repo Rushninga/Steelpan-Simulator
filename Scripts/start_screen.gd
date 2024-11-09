58c961788bf977f39d5c269f7b7c475ae8af1156
Rushes-Art <shjankie@salcc.edu.lc>
2024-11-08 23:50:35 -0500

    responsive ui & reference link to nodes

--- Scripts/start_screen.gd
+++ Scripts/start_screen.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CenterContainer
 var mode = "connect"
 var time = 0.0
 var client = ENetMultiplayerPeer.new()
@@ -7,25 +7,32 @@ var port = 1080
 var pause = 0
 signal data_send
 
+
+#link to node refrences
+@onready var user_data_menu = $Control
+@onready var connect_menu= $Control2
+@onready var user_data_menu_label = $Control/Container/Label
+
 func _ready():
-	$Control.send_data.connect(begin_data_send)
-	$Control2.network_opp.connect(begin_network_opp)
+	user_data_menu.send_data.connect(begin_data_send)
+	connect_menu.network_opp.connect(begin_network_opp)
 	multiplayer.connected_to_server.connect(connected)
 	multiplayer.connection_failed.connect(connect_fail)
 	multiplayer.server_disconnected.connect(client_disconnect)
+	
+	var size = Vector2i(820,700)
+	DisplayServer.window_set_min_size(size, 0)
 
-	#centers control node at the center of the screen
-	$Control.global_position = $Camera2D.global_position
-	$Control2.global_position = $Camera2D.global_position
+	
 	
 func _process(delta):
 	#sets control node visibility depending if the client is connected to the server
 	if mode == "connect":
-		$Control.visible = false
-		$Control2.visible = true
+		user_data_menu.visible = false
+		connect_menu.visible = true
 	else:
-		$Control.visible = true
-		$Control2.visible = false
+		user_data_menu.visible = true
+		connect_menu.visible = false
 	
 	
 func begin_data_send(username, password):
@@ -40,18 +47,12 @@ func begin_network_opp(ip):
 	
 
 func connected():
-	$Control/Container/Label.text = "clinet is connected"
-	$Control/Container/Button.disabled = false
-	$Control/Container/Button_change.disabled = false
-	$Control2/Container/Button.disabled = true
+	user_data_menu_label.text = "clinet is connected"
 	mode = "sign in"
 	
 func client_disconnect():
-	$Control/Container/Label.text = "clinet is disconnected"
-	$Control/Container/Button.disabled = true
-	$Control/Container/Button_change.disabled = true
-	$Control2/Container/Button.disabled = false
+	user_data_menu_label.text = "clinet is disconnected"
 	mode = "connect"
 
 func connect_fail():
-	$Control/Container/Label.text = "clinet has failed to connect"
+	user_data_menu_label.text = "clinet has failed to connect"

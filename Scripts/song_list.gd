c5552f2324750f84289315b38aa5126041f8cef6
Rushes-Art <shjankie@salcc.edu.lc>
2025-03-07 18:28:18 -0400

    backup

--- Scripts/song_list.gd
+++ Scripts/song_list.gd
@@ -1,4 +1,7 @@
 extends Control
+var search_term
+var length
+var search_con
 @onready var root = get_tree().current_scene
 
 
@@ -22,3 +25,26 @@ func _on_visibility_changed():
 		for i in $VBoxContainer/ScrollContainer/SongList.get_children():
 			i.queue_free()
 		root.request_download_song.rpc_id(1)
+
+
+
+func _on_search_text_changed(new_text):
+	search_term = $VBoxContainer/Top/Search.text
+	length = search_term.length()
+	
+	
+	
+	for i in $VBoxContainer/ScrollContainer/SongList.get_children():
+		search_con = i.song_name.left(length)
+		if length > 0:
+			if length <= i.song_name.length():
+				if search_term.to_upper() == search_con.to_upper():
+					i.visible = true
+				else:
+					i.visible = false
+			else:
+				i.visible = false
+		else:
+			i.visible = true
+	
+	pass # Replace with function body.
